4455008d1ea8def7c0d0482ada486d822966dd7c
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-25 01:27:59 +0900

    Improve footer styles

--- src/layouts/footer/component.tsx
+++ src/layouts/footer/component.tsx
@@ -7,7 +7,7 @@ import type { _Props } from "./types";
 export const Component: React.VFC<_Props> = (props) => (
   <footer className="fixed-bottom p-3 text-center fw-bold bg-dark">
     {props.showPlayerTitle ? (
-      <>
+      <div className={styles.contents}>
         <FontAwesomeIcon icon={["fas", "volume-up"]} size="sm" fixedWidth />
         <Link href={props.playerUrl}>
           <a className={`mx-3 text-body ${styles.title}`}>
@@ -20,9 +20,9 @@ export const Component: React.VFC<_Props> = (props) => (
           aria-label="Close"
           onClick={props.clear}
         />
-      </>
+      </div>
     ) : (
-      <>{APP_NAME}</>
+      <div className={styles.contents}>{APP_NAME}</div>
     )}
   </footer>
 );
