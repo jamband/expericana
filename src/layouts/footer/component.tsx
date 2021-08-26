0b650edcbbcb53a094788a6e51c508d39c1b5bec
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-27 00:39:41 +0900

    Fix footer styles

--- src/layouts/footer/component.tsx
+++ src/layouts/footer/component.tsx
@@ -6,30 +6,30 @@ import type { _Props } from "./types";
 
 export const Component: React.VFC<_Props> = (props) => (
   <footer className="fixed-bottom fw-bold bg-dark">
-    {props.showPlayerTitle ? (
-      <div
-        className={`p-2 d-flex align-items-center justify-content-center ${styles.contents}`}
-      >
-        <FontAwesomeIcon
-          icon={["fas", "volume-up"]}
-          size="sm"
-          fixedWidth
-          className="mx-3"
-        />
-        <Link href={props.playerUrl}>
-          <a className={`py-2 px-1 text-body text-truncate ${styles.title}`}>
-            {props.playerTitle}
-          </a>
-        </Link>
-        <button
-          type="button"
-          className="p-3 btn-close btn-sm align-text-top"
-          aria-label="Close"
-          onClick={props.clear}
-        />
-      </div>
-    ) : (
-      <div className={`p-3 text-center ${styles.contents}`}>{APP_NAME}</div>
-    )}
+    <div className={styles.contents}>
+      {props.showPlayerTitle ? (
+        <div className="p-2 d-flex align-items-center justify-content-center">
+          <FontAwesomeIcon
+            icon={["fas", "volume-up"]}
+            size="sm"
+            fixedWidth
+            className="mx-3"
+          />
+          <Link href={props.playerUrl}>
+            <a className={`py-2 px-1 text-body text-truncate ${styles.title}`}>
+              {props.playerTitle}
+            </a>
+          </Link>
+          <button
+            type="button"
+            className="p-3 btn-close btn-sm align-text-top"
+            aria-label="Close"
+            onClick={props.clear}
+          />
+        </div>
+      ) : (
+        <div className="p-3 text-center">{APP_NAME}</div>
+      )}
+    </div>
   </footer>
 );
