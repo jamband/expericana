7d8317f7526a0199d13a0cd06db146e68f8b07a6
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-31 22:22:07 +0900

    Use functional component in pages/_document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+/* eslint-disable @next/next/no-page-custom-font */
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
